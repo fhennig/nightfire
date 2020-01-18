11dc433129eb1c4ad9cbd2665e5d04c3c3c40b3c
Felix hennig <mail@felixhennig.com>
2020-01-18 03:33:02 +0000

    cleanup

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,19 +1,19 @@
 extern crate clap;
+extern crate env_logger;
 extern crate iron;
 extern crate juniper;
 extern crate juniper_iron;
+extern crate log;
 extern crate mount;
 extern crate rand;
 extern crate staticfile;
-extern crate yaml_rust;
 extern crate stoppable_thread;
-extern crate log;
-extern crate env_logger;
+extern crate yaml_rust;
 mod conf;
 mod graphql;
-mod state;
 mod models;
 mod modes;
+mod state;
 use crate::conf::Conf;
 use crate::graphql::serve;
 use crate::models::{Light, Lights, PinModel};
@@ -28,6 +28,7 @@ fn main() {
         Some(path) => Conf::new(path.to_str().unwrap()),
         None => panic!(), // TODO make this nicer
     };
+    // setup state
     let pin_model = PinModel::new(conf.all_pins(), &conf.pi_blaster_path);
     let pin_model = Arc::new(Mutex::new(pin_model));
     let lights = conf
@@ -38,26 +39,6 @@ fn main() {
     let lights = Lights::new(lights);
     let state = State::new(lights);
     let state = Arc::new(Mutex::new(state));
-    // this is doing the effect
-    /*
-    let copy = Arc::clone(&state);
-    thread::spawn(move || {
-        let mut envs = vec![
-            PinkPulse::new(Duration::from_millis(2100), vec!["light1".to_string()]),
-            PinkPulse::new(Duration::from_millis(3300), vec!["light2".to_string()]),
-            PinkPulse::new(Duration::from_millis(3900), vec!["light3".to_string()]),
-            PinkPulse::new(Duration::from_millis(4700), vec!["light4".to_string()]),
-        ];
-        let x = Duration::from_millis(30);
-        loop {
-            thread::sleep(x);
-            let mut state = copy.lock().unwrap();
-            for env in &mut envs {
-                env.update(&mut *state);
-            }
-        }
-    });
-*/
     // this is serving the GraphQL endpoint
     serve(conf.address, Arc::clone(&state));
 }
