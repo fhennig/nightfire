9d003e61d763c28b0a08dfb0337a7fdc86cdf7f2
Felix hennig <mail@felixhennig.com>
2020-03-07 01:22:40 +0000

    Enhanced debug display.

--- server/src/piston.rs
+++ server/src/piston.rs
@@ -9,7 +9,7 @@ use stoppable_thread::{spawn, StoppableHandle};
 
 pub fn run_piston_thread(state: Arc<Mutex<State>>) {
     println!("Startin window thread!");
-    let mut window: PistonWindow = WindowSettings::new("Hello World!", [512; 2])
+    let mut window: PistonWindow = WindowSettings::new("Hello World!", [300; 2])
         .exit_on_esc(true)
         .build()
         .unwrap();
@@ -17,15 +17,54 @@ pub fn run_piston_thread(state: Arc<Mutex<State>>) {
     while let Some(e) = window.next() {
         window.draw_2d(&e, |c, g, _| {
             clear([0.5, 0.5, 0.5, 1.0], g);
-            for (i, id) in LightId::all().iter().enumerate() {
-                let color = state.lock().unwrap().get_color(&id);
-                rectangle(
-                    [color.red as f32, color.green as f32, color.blue as f32, 1.0],
-                    [(i as f64) * 50.0, 0.0, 50.0, 100.0], // rectangle
-                    c.transform,
-                    g,
-                );
-            }
+
+            let stripe = [0.0, 0.0, 30.0, 100.0];
+            let color = state.lock().unwrap().get_color(&LightId::Top);
+            let color = [color.red as f32, color.green as f32, color.blue as f32, 1.0];
+
+            let transform = c
+                .transform
+                .trans(100.0, 100.0)
+                .rot_deg(135.0)
+                .trans(-15.0, 15.0);
+
+            rectangle(color, stripe, transform, g);
+
+            
+            let color = state.lock().unwrap().get_color(&LightId::Bottom);
+            let color = [color.red as f32, color.green as f32, color.blue as f32, 1.0];
+
+            let transform = c
+                .transform
+                .trans(100.0, 100.0)
+                .rot_deg(-45.0)
+                .trans(-15.0, 15.0);
+
+            rectangle(color, stripe, transform, g);
+
+            
+            let color = state.lock().unwrap().get_color(&LightId::Left);
+            let color = [color.red as f32, color.green as f32, color.blue as f32, 1.0];
+
+            let transform = c
+                .transform
+                .trans(100.0, 100.0)
+                .rot_deg(45.0)
+                .trans(-15.0, 15.0);
+
+            rectangle(color, stripe, transform, g);
+
+            
+            let color = state.lock().unwrap().get_color(&LightId::Bottom);
+            let color = [color.red as f32, color.green as f32, color.blue as f32, 1.0];
+
+            let transform = c
+                .transform
+                .trans(100.0, 100.0)
+                .rot_deg(-135.0)
+                .trans(-15.0, 15.0);
+
+            rectangle(color, stripe, transform, g);
         });
     }
 }
