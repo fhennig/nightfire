7afbe6fa6c736892c32a5c90b44e3d5280bd301f
Felix hennig <mail@felixhennig.com>
2020-04-02 00:19:31 +0100

    implemented lumi_jack

--- server/src/bin/lumi_jack.rs
+++ server/src/bin/lumi_jack.rs
@@ -1,6 +1,17 @@
 use lumi::osc::OscSender;
 use lumi::jack::read_audio;
 
-fn main() {
-    println!("Hello!");
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let updater = Box::new(OscSender::new(
+        "0.0.0.0:33768".parse().unwrap(),
+        "0.0.0.0:33766".parse().unwrap(),
+    ));
+    let _client = read_audio(updater);
+    // a silly loop to keep the thread open
+    loop {
+        let dur = std::time::Duration::from_millis(10000);
+        std::thread::sleep(dur);
+    }
+    Ok(())
 }
+
