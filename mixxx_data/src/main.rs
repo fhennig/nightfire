225b2e1566cf8e9f5621711181840cc0f87c8cc0
Felix hennig <mail@felixhennig.com>
2020-04-20 22:23:30 +0100

    Added arg parsing for signal processing parameters

--- mixxx_data/src/main.rs
+++ mixxx_data/src/main.rs
@@ -109,13 +109,12 @@ fn write_out(out_file: String, track_info: &TrackInfo, hist: &Vec<Vec<f32>>, tar
 
 /// Reads the track from file, processes it, generates targets and
 /// writes both to a pickle file named out_file.
-fn process_track(track_info: &TrackInfo, out_file: String) {
+fn process_track(track_info: &TrackInfo, out_file: String, params: &ProcessingParams) {
     let file = File::open(track_info.loc()).expect("Could not open track file.");
     let source = rodio::Decoder::new(BufReader::new(file)).expect("Could not parse track file.");
     let sample_rate = source.sample_rate();
     println!("sample_rate: {}", source.sample_rate());
-    let mut processor =
-        nfa::SignalProcessor::new(sample_rate as f32, 20., 20_000., 3., 30, 50., None);
+    let mut processor = params.get_processor(sample_rate as f32);
     let channels = source.channels() as usize;
     let ch1 = source.step_by(channels);
     let mut samples = 0;
@@ -142,13 +141,91 @@ fn process_track(track_info: &TrackInfo, out_file: String) {
 }
 
 fn get_args() -> clap::ArgMatches<'static> {
-    clap_app!(myapp =>
-    (about: "Does awesome things")
-    (@arg DB_FILE: --database
-     +takes_value
-     "Mixxx DB location (default: ~/.mixxx/mixxxdb.sqlite)")
+    clap_app!(
+        myapp =>
+            (about: "Does awesome things")
+            (@arg DB_FILE: --database
+             +takes_value
+             "Mixxx DB location (default: ~/.mixxx/mixxxdb.sqlite)")
+            (@arg F_LOW: -l --low
+             +takes_value
+             +required
+             "lowest frequency to capture."
+            )
+            (@arg F_HIGH: -h --high
+             +takes_value
+             +required
+             "lowest frequency to capture."
+            )
+            (@arg Q: -q
+             +takes_value
+             +required
+             "highest frequency to capture."
+            )
+            (@arg NUM_FILTERS: -k --num_filters
+             +takes_value
+             +required
+             "number of frequency bands to capture."
+            )
+            (@arg RATE: -r --rate
+             +takes_value
+             +required
+             "subsampling rate in Hz."
+            )
     )
     .get_matches()
+}
+
+struct ProcessingParams {
+    low: f32,
+    high: f32,
+    q: f32,
+    n_filters: usize,
+    rate: f32,
+}
+
+impl ProcessingParams {
+    pub fn from_args(args: &clap::ArgMatches<'static>) -> ProcessingParams {
+        ProcessingParams {
+            low: args
+                .value_of("F_LOW")
+                .unwrap()
+                .parse::<f32>()
+                .expect("Could not parse 'low' argument."),
+            high: args
+                .value_of("F_HIGH")
+                .unwrap()
+                .parse::<f32>()
+                .expect("Could not parse 'high' argument."),
+            q: args
+                .value_of("Q")
+                .unwrap()
+                .parse::<f32>()
+                .expect("Could not parse 'q' argument."),
+            n_filters: args
+                .value_of("NUM_FILTERS")
+                .unwrap()
+                .parse::<usize>()
+                .expect("Could not parse 'num_filters' argument."),
+            rate: args
+                .value_of("RATE")
+                .unwrap()
+                .parse::<f32>()
+                .expect("Could not parse 'rate' argument."),
+        }
+    }
+
+    pub fn get_processor(&self, sample_rate: f32) -> nfa::SignalProcessor {
+        nfa::SignalProcessor::new(
+            sample_rate,
+            self.low,
+            self.high,
+            self.q,
+            self.n_filters,
+            self.rate,
+            None,
+        )
+    }
 }
 
 fn default_db_file() -> PathBuf {
@@ -168,6 +245,8 @@ fn main() {
     if !db_file.exists() {
         panic!("Database file not found!");
     }
+    // parse signal processing params
+    let params = ProcessingParams::from_args(&args);
     // load track infos from DB
     let tracks = load_track_info(db_file);
     // select only 128 BPM tracks
@@ -180,6 +259,6 @@ fn main() {
     // do processing
     for (i, track) in tracks.iter().enumerate() {
         println!("{}", track.title);
-        process_track(&track, format!("data/{}.pickle", i));
+        process_track(&track, format!("data/{}.pickle", i), &params);
     }
 }
