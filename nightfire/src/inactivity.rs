cb2f5f8b036f1480fa38d67d4740e9e57001300f
Felix hennig <mail@felixhennig.com>
2020-06-19 00:31:41 +0100

    Added InactivityTracker

---
+++ nightfire/src/inactivity.rs
@@ -0,0 +1,26 @@
+use std::time::{Duration, SystemTime};
+
+/// A struct that helps to keep track of how long ago the last
+/// interaction was.  If it was longer ago than period X, the state
+/// switches from active to inactive.
+pub struct InactivityTracker {
+    t_last_active: SytemTime,
+    wait_duration: Duration,
+}
+
+impl InactivityTracker {
+    pub fn new() -> InactivityTracker {
+        InactivityTracker {
+            t_last_active: SystemTime::UNIX_EPOCH,
+            wait_duration: Duration::from_secs(10),
+        }
+    }
+
+    pub fn register_activity(&mut self) {
+        self.t_last_active = SystemTime::now();
+    }
+
+    pub fn is_inactive(self) -> bool {
+        return SystemTime::now().duration_since(self.t_last_active) > self.wait_duration;
+    }
+}
