9290ed37169860ed1b0afcdae6a2b3e07bb02da0
Felix hennig <mail@felixhennig.com>
2020-09-25 23:26:13 +0200

    Added new intensity display to nf_eq

--- nf_eq/src/ui.rs
+++ nf_eq/src/ui.rs
@@ -5,20 +5,24 @@ use std::sync::{Arc, Mutex};
 use std::vec::Vec;
 
 pub struct DisplayData {
-    pub frequency_bins: Vec<f32>
+    pub frequency_bins: Vec<f32>,
+    pub audio_features: audio::AudioFeatures,
+    pub normalize: bool,
 }
 
 impl DisplayData {
-    pub fn new(frequency_bin_count: usize) -> DisplayData {
+    pub fn new(frequency_bin_count: usize, normalize: bool) -> DisplayData {
         return DisplayData {
-            frequency_bins: vec![0.; frequency_bin_count]
-        }
+            frequency_bins: vec![0.; frequency_bin_count],
+            audio_features: audio::AudioFeatures::new(),
+            normalize: normalize
+        };
     }
 }
 
 pub struct EqViz {
     sig_proc: audio::SigProc<audio::DefaultSampleHandler>,
-    display_data: Arc<Mutex<DisplayData>>
+    display_data: Arc<Mutex<DisplayData>>,
 }
 
 impl EqViz {
@@ -26,7 +30,7 @@ impl EqViz {
         let n = signal_processor.filter.num_filters();
         EqViz {
             sig_proc: signal_processor,
-            display_data: Arc::new(Mutex::new(DisplayData::new(n))),
+            display_data: Arc::new(Mutex::new(DisplayData::new(n, false))),
         }
     }
 
@@ -37,8 +41,8 @@ impl EqViz {
 
 impl nf_audio::ValsHandler for EqViz {
     fn take_frame(&mut self, frame: &[f32]) {
-        // Here we receive raw audio frames.  They are added to the signal processor and 
-        // afterwards we read the latest frequency bin values and set them into our 
+        // Here we receive raw audio frames.  They are added to the signal processor and
+        // afterwards we read the latest frequency bin values and set them into our
         // internal state.
 
         // println!("{}", frame.len());
@@ -49,7 +53,8 @@ impl nf_audio::ValsHandler for EqViz {
             vals.push(self.sig_proc.sample_handler.get_filter_decayed(i));
         }
         let mut curr_data = self.display_data.lock().unwrap();
-        *curr_data = DisplayData { frequency_bins: vals };
+        curr_data.frequency_bins = vals;
+        curr_data.audio_features = self.sig_proc.sample_handler.curr_feats;
     }
 }
 
@@ -60,6 +65,7 @@ fn itof(color_intensity: i32) -> f32 {
 pub fn create_window(display_data: Arc<Mutex<DisplayData>>) {
     let bg_color = [itof(237), itof(235), itof(223), 1.0];
     let fg_color = [itof(64), itof(216), itof(133), 1.0];
+    let alt_fg_color = [itof(216), itof(64), itof(133), 1.0];
     let mut window: PistonWindow = WindowSettings::new("Hello Piston!", [500, 300])
         .exit_on_esc(true)
         .build()
@@ -83,6 +89,13 @@ pub fn create_window(display_data: Arc<Mutex<DisplayData>>) {
                     graphics,
                 );
             }
+            let int = dd.audio_features.intensity as f64;
+            rectangle(
+                alt_fg_color,
+                [(1. - int) * w / 2., 0., int * w, 40.],
+                context.transform,
+                graphics,
+            );
         });
     }
 }
