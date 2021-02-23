970e7d30a04f39a2c658d3f8a8f1673c96b900bf
Felix Hennig <mail@felixhennig.com>
2021-02-23 21:39:11 +0100

    Added Volumne Plot

--- nf_monitor/src/ui.rs
+++ nf_monitor/src/ui.rs
@@ -16,7 +16,9 @@ pub struct MonitorData {
     onset_scores: VecDeque<f32>,
     onset_means: VecDeque<f32>,
     onset_stddevs: VecDeque<f32>,
-    onset_threshold: VecDeque<f32>
+    onset_threshold: VecDeque<f32>,
+    bass_intensities: VecDeque<f32>,
+    highs_intensities: VecDeque<f32>
 }
 
 impl MonitorData {
@@ -25,7 +27,9 @@ impl MonitorData {
             onset_scores: VecDeque::from(vec![0f32; N_DATA_POINTS + 1]),
             onset_means: VecDeque::from(vec![0f32; N_DATA_POINTS + 1]),
             onset_stddevs: VecDeque::from(vec![0f32; N_DATA_POINTS + 1]),
-            onset_threshold: VecDeque::from(vec![0f32; N_DATA_POINTS + 1])
+            onset_threshold: VecDeque::from(vec![0f32; N_DATA_POINTS + 1]),
+            bass_intensities: VecDeque::from(vec![0f32; N_DATA_POINTS + 1]),
+            highs_intensities: VecDeque::from(vec![0f32; N_DATA_POINTS + 1])
         }
     }
 
@@ -36,11 +40,15 @@ impl MonitorData {
             self.onset_means.pop_front();
             self.onset_stddevs.pop_front();
             self.onset_threshold.pop_front();
+            self.bass_intensities.pop_front();
+            self.highs_intensities.pop_front();
         }
         self.onset_scores.push_back(new_feats.full_onset_score);
         self.onset_means.push_back(new_feats.full_onset_mean);
         self.onset_stddevs.push_back(new_feats.full_onset_stddev);
         self.onset_threshold.push_back(new_feats.full_onset_mean + 3. * new_feats.full_onset_stddev);
+        self.bass_intensities.push_back(new_feats.bass_intensity.current_value());
+        self.highs_intensities.push_back(new_feats.highs_intensity.current_value());
     }
 }
 
@@ -93,25 +101,49 @@ pub fn create_window(monitor_data: Arc<Mutex<MonitorData>>) {
     while let Some(_) = draw_piston_window(&mut window, |b| {
         let root = b.into_drawing_area();
         root.fill(&WHITE)?;
+        let root = root.titled("nf_monitor", ("sans-serif", 30))?;
+        let tiles = root.split_evenly((2, 1));
+        let upper = tiles.get(0).unwrap();
+        let lower = tiles.get(1).unwrap();
 
-        let mut cc = ChartBuilder::on(&root)
+        let mut cc = ChartBuilder::on(&upper)
             .margin(10)
-            .caption("Real Time Onsets", ("sans-serif", 30))
-            .x_label_area_size(40)
-            .y_label_area_size(50)
+            .caption("Volume", ("sans-serif", 20))
+            .x_label_area_size(20)
+            .y_label_area_size(25)
+            .build_cartesian_2d(0..N_DATA_POINTS as u32, 0f32..1f32)?;
+
+        cc.configure_mesh().draw()?;
+
+        // lock data once
+        let data = monitor_data.lock().unwrap();
+
+        cc.draw_series(LineSeries::new(
+            (0..).zip(data.bass_intensities.iter()).map(|(a, b)| (a, *b)),
+            &Palette99::pick(0),
+        ))?   
+        .label("Bass")
+        .legend(move |(x, y)| {
+            Rectangle::new([(x - 5, y - 5), (x + 5, y + 5)], &Palette99::pick(2))
+        });   
+
+        cc.configure_series_labels()
+        .position(SeriesLabelPosition::UpperLeft)
+        .background_style(&WHITE.mix(0.8))
+        .border_style(&BLACK)
+        .draw()?;
+        
+
+
+        let mut cc = ChartBuilder::on(&lower)
+            .margin(10)
+            .caption("Onsets", ("sans-serif", 20))
+            .x_label_area_size(20)
+            .y_label_area_size(25)
             .build_cartesian_2d(0..N_DATA_POINTS as u32, 0f32..10f32)?;
 
-        cc.configure_mesh()
-            // .x_label_formatter(&|x| format!("{}", -(LENGTH as f32) + (*x as f32 / FPS as f32)))
-            // .y_label_formatter(&|y| format!("{}%", (*y * 100.0) as u32))
-            // .x_labels(15)
-            // .y_labels(5)
-            // .x_desc("Seconds")
-            // .y_desc("% Busy")
-            // .axis_desc_style(("sans-serif", 15))
-            .draw()?;
+        cc.configure_mesh().draw()?;
 
-        let data = monitor_data.lock().unwrap();
         cc.draw_series(LineSeries::new(
             (0..).zip(data.onset_stddevs.iter()).map(|(a, b)| (a, *b)),
             &Palette99::pick(2),
