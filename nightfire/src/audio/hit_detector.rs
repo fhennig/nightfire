e64284b22675ca69a8813bba0c00fc66953caa96
Felix Hennig <mail@felixhennig.com>
2021-03-26 23:15:18 +0100

    Updated hit detector

--- nightfire/src/audio/hit_detector.rs
+++ nightfire/src/audio/hit_detector.rs
@@ -1,5 +1,7 @@
 use std::cmp::Ordering;
 use std::vec::Vec;
+use std::collections::VecDeque;
+
 
 fn get_median(v: &Vec<f32>) -> Option<f32> {
     if v.len() > 0 {
@@ -12,6 +14,8 @@ fn get_median(v: &Vec<f32>) -> Option<f32> {
 
 pub struct HitDetector {
     hit_count: usize,
+    streak_buf_size: usize,
+    time_deltas: VecDeque<f32>,
     sorted_time_deltas: Vec<f32>,
     time_since_last_hit: Option<f32>,
 }
@@ -20,6 +24,8 @@ impl HitDetector {
     pub fn new() -> Self {
         Self {
             hit_count: 0,
+            streak_buf_size: 32,
+            time_deltas: VecDeque::new(),
             sorted_time_deltas: Vec::new(),
             time_since_last_hit: None,
         }
@@ -30,16 +36,27 @@ impl HitDetector {
             self.time_since_last_hit = Some(time_passed + time_delta);
         }
         if hit {
-            println!("Hit!");
             if let Some(time_passed) = self.time_since_last_hit {
                 if time_passed > 0.2 {
+                    println!("Hit!");
                     self.hit_count += 1;
+                    if self.time_deltas.len() == self.streak_buf_size {
+                        let val = self.time_deltas.pop_back().unwrap();
+                        let pos = self
+                            .sorted_time_deltas
+                            .binary_search_by(|a| a.partial_cmp(&val).unwrap_or(Ordering::Equal))
+                            .unwrap_or_else(|e| e);
+                        println!("{:?}", self.time_deltas);
+                        println!("{:?}", self.sorted_time_deltas);
+                        println!("{:?}", val);
+                        self.sorted_time_deltas.remove(pos);
+                    }
+                    self.time_deltas.push_front(time_passed);
                     let pos = self
                         .sorted_time_deltas
                         .binary_search_by(|a| a.partial_cmp(&time_passed).unwrap_or(Ordering::Equal))
                         .unwrap_or_else(|e| e);
                     self.sorted_time_deltas.insert(pos, time_passed);
-                    self.sorted_time_deltas.push(time_passed);
                 }
             } else {
                 self.hit_count += 1;
@@ -55,6 +72,7 @@ impl HitDetector {
                 println!("Streak of len {:?} is over!", self.hit_count);
                 self.hit_count = 0;
                 self.sorted_time_deltas.clear();
+                self.time_deltas.clear();
                 self.time_since_last_hit = None;
             }
         }
