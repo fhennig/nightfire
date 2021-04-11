0158d46434310be79b41a3ae1f850b366f8344b6
Felix Hennig <mail@felixhennig.com>
2021-04-11 22:04:41 +0200

    Removed more dead code

--- nightfire/src/audio/processors/mod.rs
+++ nightfire/src/audio/processors/mod.rs
@@ -1,7 +1,5 @@
-mod intensity;
 mod phrase_detector;
 pub mod primitives;
 pub mod running_stats;
-pub use intensity::IntensityTracker;
 pub use phrase_detector::PhraseDetector;
 pub use running_stats::RunningStats;
