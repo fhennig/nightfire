6175cadb367d9d2ea03c9d2a768a396e0bf4693d
Felix Hennig <mail@felixhennig.com>
2021-04-30 19:38:03 +0200

    HighLow refactoring

--- nf_lichtspiel/src/mode/high_low.rs
+++ nf_lichtspiel/src/mode/high_low.rs
@@ -1,7 +1,7 @@
 use crate::mode::Mode;
 use crate::util::controller_coordinate_to_coordinate;
 use dualshock3::Controller;
-use nightfire::audio;
+use nightfire::audio::{intensity::IntensityID, AudioEvent2, SignalProcessor};
 use nightfire::light::color::{Color, ColorsExt};
 use nightfire::light::cmap::{ManualMode, StaticSolidMap};
 /// Idea: 2 blobs of color, one red one blue, one controlled with each stick.
@@ -15,8 +15,10 @@ pub struct HighLow {
     color: Layer<ManualMode, DiscretePosMask>,
     left_blob: SolidLayer<PosMask>,
     right_blob: SolidLayer<PosMask>,
-    signal_processor: audio::SigProc<audio::DefaultSampleHandler>,
-    speed_no: f32,
+    signal_processor: SignalProcessor,
+    speed_no: usize,
+    various_speed_intensity_ids: Vec<IntensityID>,
+    is_silence: bool,
 }
 
 impl HighLow {
@@ -26,51 +28,31 @@ impl HighLow {
         m.set_top(Color::blue());
         m.set_bottom(Color::red());
         // setup audio
-        let filter = audio::SignalFilter::new(20., 20_000., sample_rate, 3., 30);
-        let sample_freq = 50.;
-        let handler = audio::DefaultSampleHandler::new(sample_freq, filter.freqs.clone());
-        let proc = audio::SigProc::<audio::DefaultSampleHandler>::new(
-            sample_rate,
-            filter,
-            sample_freq,
-            handler,
-        );
+        let fps = 50.;
         let blob_color = Color::new(1., 0.8, 0.05);
         HighLow {
             color: Layer::new(m, DiscretePosMask::new(1., 1., 1., 1.)),
             left_blob: SolidLayer::new(StaticSolidMap::new(blob_color), PosMask::new()),
             right_blob: SolidLayer::new(StaticSolidMap::new(blob_color), PosMask::new()),
-            signal_processor: proc,
-            speed_no: 3f32,
+            signal_processor: SignalProcessor::new(sample_rate, fps),
+            speed_no: 1,
+            various_speed_intensity_ids: vec![
+                IntensityID::get("bass_speed01"),
+                IntensityID::get("bass_speed02"),
+                IntensityID::get("bass_speed03"),
+            ],
+            is_silence: true,
         }
     }
 
     pub fn audio_decay_faster(&mut self) {
-        self.speed_no = 0f32.max(10f32.min(self.speed_no + 1f32));
-        self.signal_processor
-            .sample_handler
-            .curr_feats
-            .bass_intensity
-            .decay_factor = (-self.speed_no).exp();
-        self.signal_processor
-            .sample_handler
-            .curr_feats
-            .highs_intensity
-            .decay_factor = (-self.speed_no).exp();
+        self.speed_no = self.speed_no - 1;
+        self.speed_no = self.speed_no.min(0);
     }
 
     pub fn audio_decay_slower(&mut self) {
-        self.speed_no = 0f32.max(10f32.min(self.speed_no - 1f32));
-        self.signal_processor
-            .sample_handler
-            .curr_feats
-            .bass_intensity
-            .decay_factor = (-self.speed_no).exp();
-        self.signal_processor
-            .sample_handler
-            .curr_feats
-            .highs_intensity
-            .decay_factor = (-self.speed_no).exp();
+        self.speed_no = self.speed_no + 1;
+        self.speed_no = self.speed_no.max(self.various_speed_intensity_ids.len());
     }
 }
 
@@ -96,25 +78,27 @@ impl Mode for HighLow {
     }
 
     fn audio_update(&mut self, frame: &[f32]) {
-        self.signal_processor.add_audio_frame(frame);
-        let mut bass_intensity = self
-            .signal_processor
-            .sample_handler
-            .curr_feats
-            .bass_intensity
-            .current_value();
-        let mut highs_intensity = self
-            .signal_processor
-            .sample_handler
-            .curr_feats
-            .highs_intensity
-            .current_value();
-        if self.signal_processor.sample_handler.curr_feats.is_silence() {
-            bass_intensity = 1.0;
-            highs_intensity = 1.0;
+        let events = self.signal_processor.add_audio_frame(frame);
+        for event in events {
+            match event {
+                AudioEvent2::Intensities(intensities) => {
+                    let mut highs_intensity = *intensities.get(&IntensityID::get("highs")).unwrap();
+                    if self.is_silence {
+                        highs_intensity = 1.0;
+                    }
+                    self.color.mask.set_top(highs_intensity.into());
+                    let mut bass_intensity = *intensities.get(&IntensityID::get("bass")).unwrap();
+                    if self.is_silence {
+                        bass_intensity = 1.0;
+                    }
+                    self.color.mask.set_bottom(bass_intensity.into());
+                    
+                }
+                AudioEvent2::SilenceStarted => self.is_silence = true,
+                AudioEvent2::SilenceEnded => self.is_silence = false,
+                _ => (),
+            }
         }
-        self.color.mask.set_top(highs_intensity.into());
-        self.color.mask.set_bottom(bass_intensity.into());
     }
 
     fn periodic_update(&mut self) {}
