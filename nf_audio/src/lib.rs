071c3efd9a72752eef0b44a8d3f58ab136372cd2
Felix Hennig <mail@felixhennig.com>
2021-05-02 00:20:20 +0200

    Removed jack support from nf_audio

--- nf_audio/src/lib.rs
+++ nf_audio/src/lib.rs
@@ -4,13 +4,11 @@
 //! as well as support for JACK.
 use cpal::traits::{DeviceTrait, EventLoopTrait, HostTrait};
 use cpal::{StreamData, UnknownTypeInputBuffer};
-use jack::{AsyncClient, AudioIn, Client, Control, Port, ProcessHandler, ProcessScope};
 use log::info;
 use stoppable_thread::spawn;
 
 /// Which audio backend to use, and specific backend parameters
 pub enum AudioParameters {
-    Jack(String),
     Cpal,
 }
 
@@ -29,13 +27,8 @@ impl dyn AudioGetter {
     /// Creates a new audio input with the given parameters
     pub fn new(params: &AudioParameters) -> Box<dyn AudioGetter> {
         match params {
-            AudioParameters::Jack(port) => AudioGetter::new_jack("nf_eq", &port),
             AudioParameters::Cpal => AudioGetter::new_cpal(),
         }
-    }
-
-    pub fn new_jack(client_name: &str, port_name: &String) -> Box<dyn AudioGetter> {
-        Box::new(JackAudioGetter::new(client_name, port_name))
     }
 
     pub fn new_cpal() -> Box<dyn AudioGetter> {
@@ -95,122 +88,4 @@ impl AudioGetter for CpalAudioGetter {
     }
 
     fn stop_processing(&mut self) {}
-}
-
-pub struct JackAudioGetter {
-    client: Option<jack::Client>,
-    active_client: Option<jack::AsyncClient<(), JackHandler>>,
-    port: String,
-}
-
-impl JackAudioGetter {
-    fn new(client_name: &str, port_name: &String) -> JackAudioGetter {
-        JackAudioGetter {
-            client: Some(
-                jack::Client::new(client_name, jack::ClientOptions::empty())
-                    .expect("Failed to open jack client.")
-                    .0,
-            ),
-            active_client: None,
-            port: port_name.to_string(),
-        }
-    }
-}
-
-impl AudioGetter for JackAudioGetter {
-    fn get_sample_rate(&self) -> f32 {
-        self.client.as_ref().unwrap().sample_rate() as f32
-    }
-
-    fn start_processing(&mut self, vals_handler: Box<dyn ValsHandler>) {
-        let name = self.client.as_ref().unwrap().name();
-        let spec = jack::AudioIn::default();
-        let audio_in_port = self
-            .client
-            .as_ref()
-            .unwrap()
-            .register_port("in", spec)
-            .expect("Failed to register port.");
-
-        let port_name = format!("{}:{}", name, "in");
-
-        let p_handler = JackHandler::new(audio_in_port, vals_handler);
-
-        let active_client = self
-            .client
-            .take()
-            .unwrap()
-            .activate_async((), p_handler)
-            .unwrap();
-        info!("Async processhandling started.");
-
-        active_client
-            .as_client()
-            .connect_ports_by_name(&self.port, &port_name)
-            .expect("Failed to connect client to audio in port");
-        self.active_client = Some(active_client);
-    }
-
-    fn stop_processing(&mut self) {}
-}
-
-pub struct JackHandler {
-    audio_in_port: Port<AudioIn>,
-    vals_handler: Box<dyn ValsHandler>,
-}
-
-impl JackHandler {
-    fn new(audio_in_port: Port<AudioIn>, handler: Box<dyn ValsHandler>) -> JackHandler {
-        JackHandler {
-            audio_in_port: audio_in_port,
-            vals_handler: handler,
-        }
-    }
-}
-
-impl ProcessHandler for JackHandler {
-    fn process(&mut self, client: &Client, process_scope: &ProcessScope) -> Control {
-        // read frame from the port
-        let audio = self.audio_in_port.as_slice(process_scope);
-        // give it to the handler
-        self.vals_handler.take_frame(audio);
-        // print CPU load
-        println!("{}", client.cpu_load());
-        // Continue the loop
-        Control::Continue
-    }
-}
-
-pub fn open_client(name: &str) -> jack::Client {
-    info!("Starting processing.  Creating client and port ...");
-    jack::Client::new(name, jack::ClientOptions::empty())
-        .expect("Failed to open jack client.")
-        .0
-}
-
-pub fn start_processing(
-    client: jack::Client,
-    port: &str,
-    vals_handler: Box<dyn ValsHandler>,
-) -> AsyncClient<(), JackHandler> {
-    let name = client.name();
-
-    let spec = jack::AudioIn::default();
-    let audio_in_port = client
-        .register_port("in", spec)
-        .expect("Failed to register port.");
-
-    let port_name = format!("{}:{}", name, "in");
-
-    let p_handler = JackHandler::new(audio_in_port, vals_handler);
-
-    let active_client = client.activate_async((), p_handler).unwrap();
-    info!("Async processhandling started.");
-
-    active_client
-        .as_client()
-        .connect_ports_by_name(port, &port_name)
-        .expect("Failed to connect client to audio in port");
-
-    active_client
 }
